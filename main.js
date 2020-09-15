426445330a5e609e48bada98562ee242187b9ea1
Christopher Dubbs <Christopher.Dubbs@usdigital.com>
2020-09-15 08:51:10 -0700

    maintenance commit

--- main.js
+++ main.js
@@ -9,7 +9,10 @@ function createMainWindow() {
 		height: 600,
 	});
 
-	mainWindow.loadURL(`https://twitter.com`);
+	//mainWindow.loadURL(`https://twitter.com`);
+	// Current Directory: __dirname
+	//mainWindow.loadURL(`file://${__dirname}/app/index.html`);
+	mainWindow.loadFile('./app/index.html');
 }
 
 app.on('ready', createMainWindow);
